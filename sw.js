1f568bdf8a2f209634b397ed1dd4f7edca3fa2ea
Juutilainen26 <ee5213_@naver.com>
2026-08-07 21:22:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE = 'gagyebu-v2';
+const CACHE = 'gagyebu-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
   './icon.svg',
+  './icon-192.png',
+  './icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js'
 ];
